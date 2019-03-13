e5929d2f9592429c024e68e3018672e74394013d
beven <beven.zhou@hotmail.com>
2019-03-13 21:06:49 +0800

    修复windows下路径问题

--- src/loader.js
+++ src/loader.js
@@ -1,7 +1,7 @@
 module.exports = function () {
   this.cacheable && this.cacheable();
   return ` 
-    import AutoReloadIcon from '${__dirname}/runtime.js';
+    import AutoReloadIcon from '${__dirname.replace(/\\/g,'/')}/runtime.js';
     import('!!./dist.js').then((allIcons) => { 
         AutoReloadIcon.load(allIcons); 
     }); 
