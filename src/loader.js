861c22d0888bba9720814c28044bcca2f5fcec8d
beven <beven.zhou@hotmail.com>
2019-06-19 16:31:13 +0800

    添加chunkName配置

--- src/loader.js
+++ src/loader.js
@@ -1,10 +1,11 @@
 module.exports = function () {
   this.cacheable && this.cacheable();
+  const options = this.query || { chunkName: 'antd-icons' };
   return ` 
     import AutoReloadIcon from 'webpack-ant-icon-loader/src/runtime.js';
     // 加载script标签会阻塞js执行，antd部分组件（如表格）无法在一轮循环内完成完整的渲染，故延时
     setTimeout(function(){
-      import('!!./dist.js').then(function(allIcons) { 
+      import(/* webpackChunkName: "${options.chunkName}" */'!!./dist.js').then(function(allIcons) { 
         AutoReloadIcon.load(allIcons); 
       }); 
     });
