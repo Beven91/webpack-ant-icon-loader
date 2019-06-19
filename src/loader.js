00667b9f6560b89ae20d84ecb4cc64178f1c6399
beven <beven.zhou@hotmail.com>
2019-06-19 15:10:29 +0800

    修改lambda表达式

--- src/loader.js
+++ src/loader.js
@@ -3,7 +3,7 @@ module.exports = function () {
   return ` 
     import AutoReloadIcon from 'webpack-ant-icon-loader/src/runtime.js';
     // 加载script标签会阻塞js执行，antd部分组件（如表格）无法在一轮循环内完成完整的渲染，故延时
-    setTimeout(() => {
+    setTimeout(function(){
       import('!!./dist.js').then(function(allIcons) { 
         AutoReloadIcon.load(allIcons); 
       }); 
